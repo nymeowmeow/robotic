301bc4dad31a8384411a825ca4fdb2e2fcfbe743
meow <pookc@hotmail.com>
2018-04-09 21:19:21 -0400

    only allow move in 4 directions

--- wk1/ShortestPath.py
+++ wk1/ShortestPath.py
@@ -36,7 +36,7 @@ class PriorityQueue(object):
             if node is not self.REMOVED:
                 del self.locationmap[node]
                 return priority, node
-        raise keyError('pop from empty prority queue')
+        raise KeyError('pop from empty prority queue')
 
     def size(self):
         return len(self.locationmap)
@@ -47,7 +47,28 @@ def indexToLoc(index, xspacing, yspacing):
 
 def locToIndex(loc, xspacing, yspacing):
     return (int(np.rint(loc[1]/yspacing - 0.5)), int(np.rint(loc[0]/xspacing - 0.5)))
-    
+
+def findValidNode(node, loc, occupancy_map, xspacing, yspacing):
+    if occupancy_map[node] == 0:
+        return node
+
+    maxrow, maxcol = occupancy_map.shape
+    closest = None
+    maxdist = sys.float_info.max
+    for delta in [(1,0), (0,1), (-1,0), (0,-1)]:
+        row = node[0] + delta[0]
+        col = node[1] + delta[1]
+        if row < 0 or row >= maxrow or col < 0 or col >= maxcol or occupancy_map[(row,col)] == 1:
+            continue
+        pos = indexToLoc((row, col), xspacing, yspacing)
+        d = math.sqrt((pos[0] - loc[0])**2 + (pos[1] - loc[1])**2)
+        if d < maxdist:
+            maxdist = d
+            closest = (row, col)
+    if closest is None:
+        raise ValueError("can not find proper grid index for ", pos)
+    return closest
+
 def dijkstras(occupancy_map,x_spacing,y_spacing,start,goal):
     """
     Implements Dijkstra's shortest path algorithm
@@ -73,23 +94,26 @@ def dijkstras(occupancy_map,x_spacing,y_spacing,start,goal):
     
     freelist = np.where(occupancy_map == 0)
     if occupancy_map[startNode[0], startNode[1]] != 0:
-        raise ValueError("start : ({}, {}) invalid, is an obstacle".format(startNode[0], startNode[1]))
+        #raise ValueError("start : ({}, {}) invalid, is an obstacle".format(startNode[0], startNode[1]))
+        startNode = findValidNode(startNode, start, occupancy_map, x_spacing, y_spacing)
     if occupancy_map[goalNode[0], goalNode[1]] != 0:
-        raise ValueError("goal: ({}, {}) invalid, is an obstacle".format(goalNode[0], goalNode[1]))
-    candidate = [ [0 if freelist[0][i] == startNode[0] and freelist[1][i] == startNode[1] else sys.float_info.max, 
+        #raise ValueError("goal: ({}, {}) invalid, is an obstacle".format(goalNode[0], goalNode[1]))
+        goalNode = findValidNode(goalNode, goal, occupancy_map, x_spacing, y_spacing)
+    candidate = [ [sys.float_info.max, 
                    i, (freelist[0][i], freelist[1][i])] for i in range(len(freelist[0]))] 
     visited = set([])
     queue = PriorityQueue(candidate)
     paths = {}
     found = False
-    viisted = set()
+
     #update initial cost
     queue.remove(startNode)
     queue.insert(startNode, initialcost)
-    updateInitial(occupancy_map, ROWS, COLS, start, startNode, 0, 1, queue, x_spacing, y_spacing)
-    #updateInitial(occupancy_map, ROWS, COLS, start, startNode, 0, -1, queue, x_spacing, y_spacing)
-    #updateInitial(occupancy_map, ROWS, COLS, start, startNode, 1, 0, queue, x_spacing, y_spacing)
-    #updateInitial(occupancy_map, ROWS, COLS, start, startNode, -1, 0, queue, x_spacing, y_spacing)
+    paths[startNode] = None
+    updateInitial(occupancy_map, ROWS, COLS, start, startNode, 0, 1, queue, paths, x_spacing, y_spacing, initialcost)
+    updateInitial(occupancy_map, ROWS, COLS, start, startNode, 0, -1, queue, paths, x_spacing, y_spacing, initialcost)
+    updateInitial(occupancy_map, ROWS, COLS, start, startNode, 1, 0, queue, paths, x_spacing, y_spacing, initialcost)
+    updateInitial(occupancy_map, ROWS, COLS, start, startNode, -1, 0, queue, paths, x_spacing, y_spacing, initialcost)
     while queue.size() > 0:
         priority, current = queue.pop()
         if current == goalNode:
@@ -106,13 +130,13 @@ def dijkstras(occupancy_map,x_spacing,y_spacing,start,goal):
         raise ValueError("fail to find shortest path")
     node = goalNode
     shortestpath = []
-    while node != startNode:
+    while node is not None:
         shortestpath.append(node)
         node = paths[node]
-    shortestpath.append(startNode)
-    print (startNode)
-    print ('*', list(reversed(shortestpath)))
-    print (goalNode)
+    #shortestpath.append(startNode)
+    #print (startNode)
+    #print ('*', list(reversed(shortestpath)))
+    #print (goalNode)
     p = list(reversed([ indexToLoc(n, x_spacing, y_spacing) for n in shortestpath]))
     #start and final position may not fall on center of the grid
     if abs(p[0][0] - start[0]) > 0.0005 or abs(p[0][1] - start[1]) > 0.0005:
@@ -123,7 +147,8 @@ def dijkstras(occupancy_map,x_spacing,y_spacing,start,goal):
     print (res)
     return res
 
-def updateInitial(occupancy_map, maxrow, maxcol, start, startNode, deltax, deltay, queue, x_spacing, y_spacing):
+def updateInitial(occupancy_map, maxrow, maxcol, start, startNode, deltax, deltay, queue, paths, 
+                  x_spacing, y_spacing, initialcost):
     x = startNode[0] + deltax
     y = startNode[1] + deltay
     #check if updated position is out of bound
@@ -133,17 +158,29 @@ def updateInitial(occupancy_map, maxrow, maxcol, start, startNode, deltax, delta
     if occupancy_map[x, y] == 1:
         return
 
+    startpos = indexToLoc((startNode[0], startNode[1]), x_spacing, y_spacing)
     deltaloc = indexToLoc((x,y), x_spacing, y_spacing)
-    delta = math.sqrt((deltaloc[0] - start[0])**2 + (deltaloc[1] - start[1])**2)  
+
+    if deltay == 0 and abs(start[0][0] - deltaloc[0]) > 1e-3:
+        return
+    elif deltax == 0 and abs(start[1][0] - deltaloc[1]) > 1e-3:
+        return
+    delta = math.sqrt((deltaloc[0] - start[0][0])**2 + (deltaloc[1] - start[1][0])**2) 
+    delta1 = math.sqrt((deltaloc[0] - startpos[0])**2 + (deltaloc[1] - startpos[1])**2) + initialcost 
     queue.remove((x,y))
-    queue.insert((x,y), delta) 
+    if delta < delta1:
+        queue.insert((x,y), delta) 
+        paths[(x,y)] = None
+    else:
+        queue.insert((x,y), delta1)
+        paths[(x,y)] = startNode
 
 def update(occupancy_map, maxrow, maxcol, loc, deltax, deltay, priority, queue, paths, visited,
             x_spacing, y_spacing):
     x = loc[0] + deltax
     y = loc[1] + deltay
     #check if updated position is out of bound
-    if x < 0 or x > maxrow or y < 0 or y > maxcol:
+    if x < 0 or x >= maxrow or y < 0 or y >= maxcol:
         return
     #check if it is an obstacle
     if occupancy_map[x, y] == 1:
@@ -291,5 +328,6 @@ def main():
 
 if __name__ == '__main__':
     #main()
-    test()
+    #test()
+    test_for_grader()
 
